fca4b117ccdd74d73b60951b6c60a799372fa1d1
Tiago Barbosa <tfreirebar@gmail.com>
2018-07-21 20:22:26 -0300

    ex8

---
+++ src/Ex05_Enum.kt
@@ -0,0 +1,16 @@
+
+enum class Color {
+    RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET
+}
+
+enum class ColorProperty(val r: Int, val g: Int, val b: Int){
+    RED(255,0,0), ORANGE(255,165,0),
+    YELLOW(255,255,0), GREEN(0,255,0), BLUE(0,0,255),
+    INDIGO(75,0,130), VIOLET(238,130,238);
+
+    fun rgb() = (r * 256 + g) * 256 + b
+}
+
+fun main(args: Array<String>){
+    println(ColorProperty.BLUE.rgb())
+}
---
+++ src/Ex06_When.kt
@@ -0,0 +1,38 @@
+
+fun getMnemonic(color: Color) =
+        when(color){
+            Color.RED -> "Richard"
+            Color.ORANGE -> "Of"
+            Color.YELLOW -> "York"
+            Color.GREEN -> "Gave"
+            Color.BLUE -> "Battle"
+            Color.INDIGO -> "In"
+            Color.VIOLET -> "Vain"
+        }
+
+fun getWarmth(color: Color) = when(color){
+    Color.RED, Color.ORANGE, Color.YELLOW -> "warm"
+    Color.GREEN -> "neutral"
+    Color.BLUE, Color.INDIGO, Color.VIOLET -> "cold"
+}
+
+fun mix(c1: Color, c2: Color) = when(setOf(c1,c2)){
+    setOf(Color.RED, Color.YELLOW) -> Color.ORANGE
+    setOf(Color.YELLOW, Color.BLUE) -> Color.GREEN
+    setOf(Color.BLUE, Color.VIOLET) -> Color.INDIGO
+    else -> throw Exception("Dirty color")
+}
+
+fun mixOptimized(c1: Color, c2: Color) = when{
+    (c1 ==  Color.RED && c2 == Color.YELLOW) || (c1 == Color.YELLOW && c2 == Color.RED) -> Color.ORANGE
+    (c1 ==  Color.YELLOW && c2 == Color.BLUE) || (c1 == Color.BLUE && c2 == Color.YELLOW) -> Color.GREEN
+    (c1 ==  Color.BLUE && c2 == Color.VIOLET) || (c1 == Color.VIOLET && c2 == Color.BLUE) -> Color.INDIGO
+    else -> throw Exception("Dirty color")
+}
+
+fun main(args: Array<String>){
+    println(getMnemonic(Color.BLUE))
+    println(getWarmth(Color.ORANGE))
+    println(mix(Color.BLUE,Color.YELLOW))
+    println(mixOptimized(Color.BLUE,Color.YELLOW))
+}
